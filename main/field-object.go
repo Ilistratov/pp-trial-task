16e71e48897fe0b61fa3b2506b6b8510eb341267
Ilistratov <i.listratov@yandex.ru>
2021-10-25 03:46:39 +0300

    initial commit

---
+++ main/field-object.go
@@ -0,0 +1,45 @@
+components {
+  id: "field-logic"
+  component: "/main/field-logic.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "field"
+  component: "/main/field.tilemap"
+  position {
+    x: 0.0
+    y: -20.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "control-panel"
+  component: "/main/control-panel.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
